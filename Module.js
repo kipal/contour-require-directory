568e2842d996ff2cd4854d3ac422581a8c000748
kipal42 <kipal42@gmail.com>
2014-05-08 17:45:37 +0200

    deps in array.

--- Module.js
+++ Module.js
@@ -1,18 +1,18 @@
 /*******************************************************************************
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2014 Nándor Kiss
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -49,20 +49,27 @@ global.Module = function (moduleReference) {
             return this;
         }
 
+        var args = null;
+        if (arguments[0] instanceof Array) {
+            args = arguments[0];
+        } else {
+            args = arguments;
+        }
+
         var depTmp = {};
 
-        for (var i in arguments) {
-            if (!arguments.hasOwnProperty(i)) {
+        for (var i in args) {
+            if (!args.hasOwnProperty(i)) {
                 continue;
             }
 
-            depTmp = Require.isValid(arguments[i]);
+            depTmp = Require.isValid(args[i]);
 
             if (!depTmp) {
                 try {
-                    depTmp = Require.require(arguments[i]);
+                    depTmp = Require.require(args[i]);
                 } catch (e) {
-                    console.log("\n\n\nError in require '" + arguments[i] + "' module.\n\n\n");
+                    console.log("\n\n\nError in require '" + args[i] + "' module.\n\n\n");
                 }
             }
 
